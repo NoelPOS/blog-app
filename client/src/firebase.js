75a24ad2a08de85dc5422bf320ad3ef4f67cc985
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-30 02:27:46 +0700

    90% finished

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA75z6LZ3n2ya17HYm-Q_tD2MGoCi9Yb0s',
+  authDomain: 'blog-app-6b05f.firebaseapp.com',
+  projectId: 'blog-app-6b05f',
+  storageBucket: 'blog-app-6b05f.appspot.com',
+  messagingSenderId: '761232261987',
+  appId: '1:761232261987:web:fd78c5653498adedd8c956',
+  measurementId: 'G-9T6TNDJ04W',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
